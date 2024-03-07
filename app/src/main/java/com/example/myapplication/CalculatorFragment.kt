059a75514d1f526d41c86a58e553fd0f383c4f03
gourderased <gourderased@gmail.com>
2024-03-07 20:08:07 +0900

    feat : 2주차 네비게이션 과제

--- app/src/main/java/com/example/myapplication/CalculatorFragment.kt
+++ app/src/main/java/com/example/myapplication/CalculatorFragment.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.databinding.FragmentCalculatorBinding
 
 class CalculatorFragment : Fragment() {
